67211b555ea059c3c7eef7d0eb8710bf0f3a76c4
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-03-16 07:52:16 +0330

    just a chagne in container

--- container/src/components/marketing-app.js
+++ container/src/components/marketing-app.js
@@ -4,6 +4,8 @@ import React, { useRef, useEffect } from 'react';
 export default () => {
   const ref = useRef(null);
 
+  // run the marketing app in here
+
   useEffect(() => {
     mount(ref.current);
   }, []);
