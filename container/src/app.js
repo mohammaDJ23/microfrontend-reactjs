ef2e72321f2f5dc69bf8fa4a8d65a7a0752e657f
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-03-17 21:01:46 +0330

    adding prefix style name in production mode

--- container/src/app.js
+++ container/src/app.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/marketing-app';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+// to prevent collisions between default generated classname in material ui
+// we customize it
+
+// generated classnamme by default are jss1, jss2, jss3
+// and this might cause some collision between different projects
+// so by this setting classname would be co1, co2, co3, ... .
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- marketing/src/app.js
+++ marketing/src/app.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// to prevent collisions between default generated classname in material ui
+// we customize it
+
+// generated classnamme by default are jss1, jss2, jss3
+// and this might cause some collision between different projects
+// so by this setting classname would be ma1, ma2, ma3, ... .
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export default function () {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path={'/pricing'} component={Pricing} />
